224a05f3c1a85dd9446218bdd1698485476eba80
alor <alberto.ornaghi@gmail.com>
2012-04-27 17:18:27 +0200

    initial commit

---
+++ lib/em-http-server.rb
@@ -0,0 +1,9 @@
+require "em-http-server/version"
+
+module Em
+  module Http
+    module Server
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/em-http-server/version.rb
@@ -0,0 +1,7 @@
+module Em
+  module Http
+    module Server
+      VERSION = "0.0.1"
+    end
+  end
+end
